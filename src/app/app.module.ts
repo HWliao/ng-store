aab3050daf565bf87946d3f126758878384d9e12
HWliao <499606788@qq.com>
2018-05-08 17:36:25 +0800

    chore: ng add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,49 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {NgStoreModule, NgStoreService} from 'ng-store';
+import {NgStoreModule} from 'ng-store';
+import {AppNavComponent} from './app-nav/app-nav.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule,
+  MatIconModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatSidenavModule,
+  MatSortModule,
+  MatTableModule,
+  MatToolbarModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppDashComponent} from './app-dash/app-dash.component';
+import {AppTableComponent} from './app-table/app-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavComponent,
+    AppDashComponent,
+    AppTableComponent
   ],
   imports: [
     BrowserModule,
-    NgStoreModule.forRoot()
+    BrowserAnimationsModule,
+    NgStoreModule.forRoot(),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   bootstrap: [AppComponent]
 })
