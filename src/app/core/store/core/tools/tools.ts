be363ca103c3f557f751e4b4b8bbaf520c1e143e
HWliao <499606788@qq.com>
2018-11-11 22:31:58 +0800

    feat: 增加几个工具函数

--- src/app/core/store/core/tools/tools.ts
+++ src/app/core/store/core/tools/tools.ts
@@ -25,3 +25,37 @@ export const noop = () => {
 };
 
 export const returnSelf = m => m;
+
+/**
+ * @param obj The object to inspect.
+ * @returns True if the argument appears to be a plain object.
+ */
+export default function isPlainObject(obj: any): boolean {
+  if (typeof obj !== 'object' || obj === null) {
+    return false;
+  }
+
+  let proto = obj;
+  while (Object.getPrototypeOf(proto) !== null) {
+    proto = Object.getPrototypeOf(proto);
+  }
+
+  return Object.getPrototypeOf(obj) === proto;
+}
+
+/**
+ * Prints a warning in the console if it exists.
+ *
+ * @param  message The warning message.
+ */
+export function warning(message: string) {
+  if (typeof console !== 'undefined' && typeof console.error === 'function') {
+    console.error(message);
+  }
+  try {
+    // This error was thrown as a convenience so that if you enable
+    // "break on all exceptions" in your console,
+    // it would pause the execution at this line.
+    throw new Error(message);
+  } catch (e) { }
+}
