0511b3d7703b84d611a8d590645a76ab9e96d7d3
HWliao <499606788@qq.com>
2018-11-15 03:10:42 +0800

    feat: aspect 30%

--- src/app/core/store/service/weave.service.ts
+++ src/app/core/store/service/weave.service.ts
@@ -1,4 +1,31 @@
+import { Type, InjectionToken, Injectable } from '@angular/core';
+
 // aspect
 // advice 通知
 // poincut 切入点
 // weaving 编织
+// target object 目标元素
+/**
+ * 在target object的metadata关于aspect advice的key
+ * value为一个数组,内部存储了附加在target object上的aspect service/handler
+ */
+export const MD_ADVICE_ASPECT = '@@[aop]advice.aspect';
+/**
+ * 在目标对象上注册aspect
+ * @param target 目标对象
+ * @param name aspect name
+ * @param token ng di token
+ * @param handler 处理函数
+ */
+export function registerdAspect(target: Type<any>, name: string, token?: Type<any> | InjectionToken<any>, handler?: Function) {
+
+}
+
+export interface AspectAdvice {
+  name: string;
+  token?: Type<any> | InjectionToken<any>;
+  handler?: Function;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AspectService { }
