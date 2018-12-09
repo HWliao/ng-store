ac6266cd088d837104d9897dbff95453522f2d78
HWliao <499606788@qq.com>
2018-12-09 21:35:22 +0800

    feat: ...

--- projects/ngsr/src/lib/event-stream/definitions.ts
+++ projects/ngsr/src/lib/event-stream/definitions.ts
@@ -2,3 +2,7 @@
  * event stream aspect 标识
  */
 export const MD_EVENT_ASPECT = '@@[event-stream]aspect';
+/**
+ * event stream 监听者通知标识
+ */
+export const MD_EVENT_SUB_ADVICE = '@@[event-stream]subscribes';
--- projects/ngsr/src/lib/event-stream/service/event.aspect.ts
+++ projects/ngsr/src/lib/event-stream/service/event.aspect.ts
@@ -1,6 +1,7 @@
 import { Aspect } from '../../aop/aop.service';
 import { EventService } from './event.service';
 import { Injectable } from '@angular/core';
+import { getConstructor } from 'ngsr/lib/tools';
 
 @Injectable({ providedIn: 'root' })
 export class EventAspect implements Aspect {
@@ -8,6 +9,7 @@ export class EventAspect implements Aspect {
   constructor(private event: EventService) { }
 
   weave(target: any) {
+    const constructor = getConstructor(target);
     throw new Error('Method not implemented.');
   }
 }
