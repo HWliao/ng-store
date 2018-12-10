222444d6ed53b78bbc29568712b8a52eaaa622e9
HWliao <499606788@qq.com>
2018-12-10 22:41:46 +0800

    feat: subscribe annotation done

--- projects/ngsr/src/lib/event-stream/annotation/subscribe.annotation.ts
+++ projects/ngsr/src/lib/event-stream/annotation/subscribe.annotation.ts
@@ -1,13 +1,15 @@
 import { Type } from '@angular/core';
 import { DESIGN_PARAMTYPES } from '../../design-metadata';
-import { checkArgument } from '../../tools';
-import { MD_EVENT_SUB_ADVICE } from '../definitions';
+import { checkArgument, warning } from '../../tools';
+import { MD_EVENT_SUB_ADVICE, SubscribeMetadata } from '../definitions';
 
 /**
  * 订阅
  */
 export function Subscribe(EventType: Type<any>) {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
+    checkArgument(typeof target[propertyKey] === 'function', `[event-stream]subscribe 标记的${propertyKey}必须为实例方法`);
+
     const params: any[] = Reflect.getOwnMetadata(DESIGN_PARAMTYPES, target, propertyKey);
     checkArgument(
       params && params.length === 1 && params[0] === EventType,
@@ -15,7 +17,12 @@ export function Subscribe(EventType: Type<any>) {
     );
 
     const constructor = target.constructor;
-    const ads = Reflect.getOwnMetadata(MD_EVENT_SUB_ADVICE, constructor);
-
+    const ads: { [key: string]: SubscribeMetadata } = Reflect.getOwnMetadata(MD_EVENT_SUB_ADVICE, constructor) || {};
+    if (ads[<any>EventType]) {
+      warning(`[event-stream]subscribe method ${propertyKey} 已经被标记为了订阅者,不能在次标记`);
+    } else {
+      ads[<any>EventType] = { type: EventType, propertyKey: propertyKey };
+      Reflect.defineMetadata(MD_EVENT_SUB_ADVICE, ads, constructor);
+    }
   };
 }
--- projects/ngsr/src/lib/event-stream/definitions.ts
+++ projects/ngsr/src/lib/event-stream/definitions.ts
@@ -1,3 +1,5 @@
+import { Type } from '@angular/core';
+
 /**
  * event stream aspect 标识
  */
@@ -6,3 +8,16 @@ export const MD_EVENT_ASPECT = '@@[event-stream]aspect';
  * event stream 监听者通知标识
  */
 export const MD_EVENT_SUB_ADVICE = '@@[event-stream]subscribes';
+/**
+ * 订阅者元数据
+ */
+export interface SubscribeMetadata {
+  /**
+   * event type class
+   */
+  type: Type<any>;
+  /**
+   * 被标记的属性
+   */
+  propertyKey: string;
+}
